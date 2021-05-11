4e6781bc54b4df171b4315b7c5abe556d2f39fca
Eduardo Cardoso <eduardo_cardoso516@outlook.com>
2021-05-10 22:53:14 -0300

    index.php Finalizado

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
+
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <title>Jogo dos Dados</title>
 </head>
 <body>
-    <?php
-        $h_dado1;
-        $h_dado2;
-        $c_dado1;
-        $c_dado2;
-
-        $h_dado1 = rand(1,6);
-        $h_dado2 = rand(1,6);
-        $c_dado1 = rand(1,6);
-        $c_dado2 = rand(1,6);
-        
-        echo $c_dado1." | ".$c_dado2." | ".$h_dado1." | ".$h_dado2;
-    ?>
+    <div id="Titulo">
+        <h1>Rolls Dice</h1>
+    </div>
+    <div id="imagem" title="Imagem_Home">
+        <img src= "img/rolar.gif">
+    </div>
+    <div>
+        <a href="vencedor.php"><button>Jogar Dados</button></a>
+    </div>
+    <div id="Descricao">
+        <p>O jogo consiste em quem faz a maior pontuação com 
+        a soma de dois dados lançados simultaneamente</p>
+    </div>
+    
 </body>
 </html>
